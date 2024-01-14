29a835507c654d8b7893e5dce0e43d46fba11027
junghongseop <a01082372487@gmail.com>
2024-01-14 20:14:00 +0900

    add :: spring_data_jpa

---
+++ src/test/java/com/jojoudy/book/springstudy/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.jojoudy.book.springstudy.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save_board() {
+        //given
+        String title = "test_title";
+        String content = "test_content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("a01082372487@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
