5afeee450bc5c320d1120dc94917590a4e316e68
junghongseop <a01082372487@gmail.com>
2024-01-14 23:33:38 +0900

    add :: 게시물 수정/조회 api

---
+++ src/main/java/com/jojoudy/book/springstudy/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.jojoudy.book.springstudy.web.dto;
+
+import com.jojoudy.book.springstudy.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
