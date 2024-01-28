9a7de58e59792d64c998cb2531a3af1de4ac4365
junghongseop <a01082372487@gmail.com>
2024-01-28 23:33:37 +0900

    add :: 기본 페이지 (코드 다시 확인 필요)

---
+++ src/test/java/com/jojoudy/book/springstudy/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.jojoudy.book.springstudy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitConfig
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainpage_loading() {
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
