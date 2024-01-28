9451f099add9309917a665758ff8063728d73fb1
junghongseop <a01082372487@gmail.com>
2024-01-28 22:41:21 +0900

    add :: 등록일/수정일 엔티티

---
+++ src/main/java/com/jojoudy/book/springstudy/domain/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.jojoudy.book.springstudy.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
