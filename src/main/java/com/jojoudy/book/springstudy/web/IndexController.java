f4a271e4b2f311f594b8a1a56aa40fdb0f277f23
junghongseop <a01082372487@gmail.com>
2024-02-07 23:23:39 +0900

    fix :: @LoginUser로 수정

--- src/main/java/com/jojoudy/book/springstudy/web/IndexController.java
+++ src/main/java/com/jojoudy/book/springstudy/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoudy.book.springstudy.web;
 
+import com.jojoudy.book.springstudy.config.auth.LoginUser;
 import com.jojoudy.book.springstudy.config.auth.dto.SessionUser;
 import com.jojoudy.book.springstudy.service.posts.PostsService;
 import com.jojoudy.book.springstudy.web.dto.PostsResponseDto;
@@ -9,20 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
