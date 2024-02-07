4582689defcfd5dcb48195f84808a468dc5ba6b8
junghongseop <a01082372487@gmail.com>
2024-02-08 01:30:49 +0900

    add :: JPA Auditing 이동

--- src/main/java/com/jojoudy/book/springstudy/Application.java
+++ src/main/java/com/jojoudy/book/springstudy/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing JPA Auditing 활성화
 @SpringBootApplication
 @Configuration
 public class Application {
