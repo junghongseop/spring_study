654c5c739163fce15fba7cc3b5c7bc5efa90b141
junghongseop <a01082372487@gmail.com>
2024-02-09 23:29:37 +0900

    fix :: 불필요 import 삭제

--- src/main/java/com/jojoudy/book/springstudy/Application.java
+++ src/main/java/com/jojoudy/book/springstudy/Application.java
@@ -3,9 +3,7 @@ package com.jojoudy.book.springstudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing JPA Auditing 활성화
 @SpringBootApplication
 @Configuration
 public class Application {
